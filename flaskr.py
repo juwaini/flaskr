dc5feab1a721a644a3739dd3f24606013eb5905f
juwaini <juwaini@gmail.com>
2016-09-14 20:28:21 +0800

    Setp 4

--- flaskr.py
+++ flaskr.py
@@ -22,6 +22,20 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode = 'r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """
+    Initialize the database
+    """
+    init_db()
+    print('Initialized the database.')
+
 def get_db():
     """
     Opens a new database connection if there is none yet for the current
